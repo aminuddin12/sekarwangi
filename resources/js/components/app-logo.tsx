59b3b980aea0f9a2680afe2698c2fed142107dde
aminuddin12 <aminuddinadl12@icloud.com>
2026-01-04 01:23:00 +0700

    Update Logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,8 +3,8 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex size-8 items-center justify-center">
-                <AppLogoIcon className="size-8 fill-current text-white dark:text-black" />
+            <div className="flex size-10 items-center justify-center">
+                <AppLogoIcon className="size-10 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
